63f1a9b8dee7d415aa045013b6ad75a5eadd717d
spcodegen <shehansp2000@gmail.com>
2025-06-19 11:34:26 +0530

    Creating Header and style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import Todo from "./components/Todo";
-import InlineComponent from "./components/InlineComponent";
-import OutlineComponent from "./components/OutlineComponent";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <InlineComponent />
-      <OutlineComponent />
+      <Header />
+      <Todo />
     </div>
   );
 }
