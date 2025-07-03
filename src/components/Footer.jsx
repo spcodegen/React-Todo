9e51b183c3fd439b567d37fd2849deb09868fb54
spcodegen <shehansp2000@gmail.com>
2025-07-03 14:49:42 +0530

    Counting Completed & Total Todos

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./footer.module.css";
+function Footer({ completedTodos, totalTodos }) {
+  return (
+    <div className={styles.footer}>
+      <span className={styles.item}>Completed Todos: {completedTodos}</span>
+      <span className={styles.item}>Total Todos: {totalTodos}</span>
+    </div>
+  );
+}
+
+export default Footer;
